2ee7e71c8d024b9377153a15202ce33bb03662be
Vyshnavi Madhusudhan <111994644+Vyshnavi21504@users.noreply.github.com>
2026-01-24 23:45:28 +0530

    finalEdit

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ const btnEl = document.getElementById("btn");
 const quoteEl = document.getElementById("quote");
 const authorEl = document.getElementById("author");
 
-const apiURL = "https://api.quotable.io/random";
+const apiURL = "https://dummyjson.com/quotes/random";
 
 async function getQuote() {
   try {
     btnEl.innerText = "Loading...";
     btnEl.disabled = true;
-    quoteEl.innerText = "Updating...";
-    authorEl.innerText = "Updating...";
+
     const response = await fetch(apiURL);
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
     const data = await response.json();
-    const quoteContent = data.content;
-    const quoteAuthor = data.author;
-    quoteEl.innerText = quoteContent;
-    authorEl.innerText = "~ " + quoteAuthor;
-    btnEl.innerText = "Get a quote";
-    btnEl.disabled = false;
-    console.log(data);
+
+    quoteEl.innerText = `"${data.quote}"`;
+    authorEl.innerText = `— ${data.author}`;
   } catch (error) {
-    console.log(error);
-    quoteEl.innerText = "An error happened, try again later";
-    authorEl.innerText = "An error happened";
-    btnEl.innerText = "Get a quote";
+    console.error("Fetch Error:", error);
+    quoteEl.innerText = "Unable to load quote 😢";
+    authorEl.innerText = "Please try again later";
+  } finally {
+    btnEl.innerText = "Get a Quote";
     btnEl.disabled = false;
   }
 }
 
-getQuote()
-
+getQuote();
 btnEl.addEventListener("click", getQuote);
